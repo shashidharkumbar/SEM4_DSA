e1bb76a77bfe80a23283335a25fbfb93f93e9cb8
SHASHIDHAR K <131255514+shashidharkumbar@users.noreply.github.com>
2024-03-06 21:06:33 +0530

    Add files via upload

---
+++ smallestNo_File.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int asciiToDecimal(char digit) 
+{
+    return digit - '0';
+}
+
+int main() {
+    int count = 0, x=0;
+    int arr[10], i=0,c=99999;
+    char ch;
+    FILE *fptr;
+    int sum=0;
+
+    fptr = fopen("text.txt", "r");
+
+    if (fptr == NULL) 
+    {
+        printf("File not available.");
+        exit(0);
+    }
+
+    while ((ch = getc(fptr)) != EOF) 
+    {
+        if(ch==',')
+        {
+            i++;
+        }
+        else
+        {
+            ch=asciiToDecimal(ch);
+            x=10*ch;
+            ch = getc(fptr);
+            ch=asciiToDecimal(ch);
+            x=x+ch;
+            arr[i]=x;
+            count++;
+            x=0;
+        }
+    }
+
+    for(int j=0;j<count;j++)
+    {
+        sum=sum+arr[j];
+        if(c>arr[j])
+            c=arr[j];
+
+    }
+
+    printf("\n Highest No in File: %d\n", c);
+
+    fclose(fptr);
+
+    return 0;
+}
